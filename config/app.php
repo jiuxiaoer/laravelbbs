7b90d3068b996d94259e431b59fec58b1d88b8d1
jiuxiao(九霄道长) <yinshen@79xj.cn>
2021-10-27 17:48:34 +0800

    生成用户认证代码

--- config/app.php
+++ config/app.php
@@ -158,7 +158,9 @@ return [
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
+//        Illuminate\Translation\TranslationServiceProvider::class,
+    //中文语言包
+        Overtrue\LaravelLang\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
