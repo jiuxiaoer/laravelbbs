2ddac012ca3844d0e59eaa014cdf1beeea86963c
jiuxiao(九霄道长) <yinshen@79xj.cn>
2021-10-29 15:57:12 +0800

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,9 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+//用户个人页面
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+//Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+//Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+//Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
