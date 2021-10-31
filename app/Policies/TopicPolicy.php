681a9d258e3ac20f27f7ef163ea610a4715f166a
jiuxiao(九霄道长) <yinshen@79xj.cn>
2021-10-31 17:08:08 +0800

    用户可以删除自己发的话题

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -7,14 +7,13 @@ use App\Models\Topic;
 
 class TopicPolicy extends Policy
 {
-
     public function update(User $user, Topic $topic)
     {
-        return $topic->user_id == $user->id;
+        return $user->isAuthorOf($topic);
     }
 
     public function destroy(User $user, Topic $topic)
     {
-        return true;
+        return $user->isAuthorOf($topic);
     }
 }
