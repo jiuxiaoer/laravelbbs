ede96af9509d43b6b4836a30564ca6aab2229c0b
jiuxiao(九霄道长) <yinshen@79xj.cn>
2021-11-02 17:43:21 +0800

    用户可以发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,13 +9,13 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
+    public function created(Reply $reply)
+    {
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
+    }
     public function creating(Reply $reply)
     {
-        //
-    }
-
-    public function updating(Reply $reply)
-    {
-        //
+        $reply->content = clean($reply->content, 'user_topic_body');
     }
 }
