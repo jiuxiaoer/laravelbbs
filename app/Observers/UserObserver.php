cd09e868a36c97ac8185aa8824b13bfda20a7f6a
jiuxiao(九霄道长) <yinshen@79xj.cn>
2021-11-05 17:12:06 +0800

    注册用户默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -2,25 +2,15 @@
 
 namespace App\Observers;
 
-use App\Models\Topic;
 use App\Models\User;
-
-// creating, created, updating, updated, saving,
-// saved,  deleting, deleted, restoring, restored
 
 class UserObserver
 {
-    public function creating(User $user)
+    public function saving(User $user)
     {
-        //
-    }
-
-    public function updating(User $user)
-    {
-        //
-    }
-
-    public function deleted(User $user) {
-        \DB::table('topics')->where('user_id', $user->id)->delete();
+        // 这样写扩展性更高，只有空的时候才指定默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
     }
 }
