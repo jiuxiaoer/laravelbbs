4212cf57ee635cb82ff563310bc8867380f23376
jiuxiao(九霄道长) <yinshen@79xj.cn>
2021-11-05 13:50:30 +0800

    站点设置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
       由 <a href="#" target="_blank">JiuXiao</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
     </p>
 
-    <p class="float-right"><a href="mailto:yinshen@79xj.cn">联系我们</a></p>
+    <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
   </div>
 </footer>
