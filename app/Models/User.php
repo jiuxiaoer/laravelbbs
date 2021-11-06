406e7c51a12017245a9119e4189cf6b4bb168c83
jiuxiao(九霄道长) <yinshen@79xj.cn>
2021-11-06 16:06:14 +0800

    前后端头像修复

--- app/Models/User.php
+++ app/Models/User.php
@@ -90,7 +90,7 @@ class User extends Authenticatable implements MustVerifyEmailContract{
     public function setAvatarAttribute($path)
     {
         // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
-        if ( ! \Str::startsWith($path, 'http')) {
+        if ( ! \Str::startsWith($path, 'http')&&! \Str::startsWith($path, '/uploads/')) {
 
             // 拼接完整的 URL
             $path =  "/uploads/images/avatars/$path";
