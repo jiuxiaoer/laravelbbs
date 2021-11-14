55c4ef0625c3b8b5f98fde9053b68a9b087837a0
jiuxiao(九霄道长) <yinshen@79xj.cn>
2021-11-14 14:52:54 +0800

    pajx 加载js 微调

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -103,6 +103,29 @@
   <script src="{{ asset('js/jquery.caret.min.js') }}"></script>
   <script src="{{ asset('js/jquery.atwho.min.js') }}"></script>
   <script>
+    $(document).ready(function() {
+      $('.huifu').click(function() {
+        $('#reply_'+$(this).attr("id")).show()
+        if ($(this).attr("name")!==undefined){
+          $('#reply_'+$(this).attr("id")+'_content').html('<a style="color: #0d8ddb" href="/users/'+$(this).attr("user_id")+'" target="_blank">'+'@'+$(this).attr("name")+'</a>'+"&nbsp;")
+        }else {
+          $('#reply_'+$(this).attr("id")+'_content').html('')
+        }
 
+      })
+      $('.form-control').atwho({
+        at: "@",
+        callbacks: {
+          remoteFilter: function(query, callback) {
+            $.getJSON("/usersjson", {q: query}, function(data) {
+              callback(data)
+            });
+          }
+        }
+      });
+    });
+    function check(form) {
+      $('#'+$(form).attr("id")+'_textarea').html($('#'+$(form).attr("id")+'_content').html())
+    }
   </script>
 @stop
